02473307a0af7e6194e212244663406186595059
RafaRed <rafaelszcardoso@gmail.com>
2022-01-22 01:42:55 -0300

    update, login metamask + unstoppable

--- unstoppable-login/web3/src/App.js
+++ unstoppable-login/web3/src/App.js
@@ -1,42 +1,137 @@
-import logo from './logo.svg';
-import './App.css';
-import {UAuthConnector} from "@uauth/web3-react";
-import UAuth from '@uauth/js'
+import "./App.css";
+import React, { Component } from "react";
+import { useWeb3React } from "@web3-react/core";
+import { injected, walletconnect, uauth } from "./connectors";
 
+function withUseWeb3React(Component) {
+  return function WrappedComponent(props) {
+    const values = useWeb3React();
+    return <Component {...props} web3ReactHook={values} />;
+  };
+}
 
-const uauthConnector = new UAuthConnector({
-  uauth: new UAuth({
-      clientID: process.env.REACT_APP_CLIENT_ID,
-      clientSecret: process.env.REACT_APP_CLIENT_SECRET,
-      redirectUri: process.env.REACT_APP_REDIRECT_URI,
-      postLogoutRedirectUri: process.env.REACT_APP_POST_LOGOUT_REDIRECT_URI,
-      scope: 'openid wallet',
-  }),
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isConnected: false,
+      showDomain: false,
+      domain: "",
+    };
 
-})
+    this.connectMetamask = this.connectMetamask.bind(this);
+    this.connectUnstoppable = this.connectUnstoppable.bind(this);
+    this.logout = this.logout.bind(this);
+    this.listConnections = this.listConnections.bind(this);
+  }
 
-async function login() {
-  try {
-    const auth = await uauthConnector.activate()
-    console.log(auth)
-  } catch (error) {
-    console.error(error)
+  async connectMetamask() {
+    console.log(injected);
+    console.log(this.props.web3ReactHook.activate(injected, undefined, true));
+
+    this.props.web3ReactHook
+      .activate(injected, null, true)
+      .then((res) => {
+        injected
+          .getAccount()
+          .then((account) => {
+            console.log(account);
+            this.setState({
+              isConnected: true,
+            });
+          })
+          .catch((e) => {
+            alert(e);
+            console.error(e);
+          });
+      })
+      .catch((e) => {
+        alert(e);
+        console.error(e);
+      });
+  }
+
+  async connectUnstoppable() {
+    this.props.web3ReactHook
+      .activate(uauth, null, true)
+      .then(async (res) => {
+        let user = await uauth.user();
+        uauth
+          .getAccount()
+          .then((account) => {
+            this.setState({
+              isConnected: true,
+              showDomain: true,
+              domain: user.sub,
+            });
+          })
+          .catch((e) => {
+            alert(e);
+            console.error(e);
+          });
+      })
+      .catch((e) => {
+        alert(e);
+        console.error(e);
+      });
+  }
+
+  logout() {
+    this.props.web3ReactHook.deactivate();
+    injected.deactivate();
+    uauth.deactivate();
+    this.setState({
+      isConnected: false,
+      showDomain: false,
+      domain: "",
+    });
+    window.ethereum.removeListener("chainChanged", window.location.reload());
+  }
+
+  listConnections() {
+    let connectAction;
+    if (this.state.isConnected) {
+      let address;
+      let hash = this.props.web3ReactHook.account;
+      if (this.state.showDomain) {
+        address = "Unstoppable Domain:" + this.state.domain +" "+ hash;
+      } else {
+        address = "Wallet:" + hash;
+      }
+
+      connectAction = (
+        <>
+          {address}
+          <button onClick={this.logout}>Logout</button>
+        </>
+      );
+    } else {
+      connectAction = (
+        <>
+          <button
+            onClick={this.connectMetamask}
+          >
+            Login with Metamask
+          </button>
+
+          <button onClick={this.connectUnstoppable}>
+            Login with Unstoppable
+          </button>
+        </>
+      );
+    }
+    return <div>{connectAction}</div>;
+  }
+
+  render() {
+    
+    let chooseConnection = this.listConnections();
+    return (
+      <div className="loginFrame">
+        {chooseConnection}
+      </div>
+    );
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button onClick={login}>logins</button>
-        
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export default withUseWeb3React(App);
--- unstoppable-login/web3/src/index.js
+++ unstoppable-login/web3/src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { Web3ReactProvider } from "@web3-react/core"
+import { Web3Provider } from "@ethersproject/providers";
 import reportWebVitals from './reportWebVitals';
+import App from './App';
+
+
+function getLibrary(provider) {
+  const library = new Web3Provider(provider)
+  library.pollingInterval = 12000
+
+  return library
+}
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Web3ReactProvider getLibrary={getLibrary}>
+            <App />
+          </Web3ReactProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
