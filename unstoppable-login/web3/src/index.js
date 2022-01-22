815a328431f447186a2b61d1d8003203d88618f5
RafaRed <rafaelszcardoso@gmail.com>
2022-01-22 16:29:53 -0300

    Clean project

--- unstoppable-login/web3/src/index.js
+++ unstoppable-login/web3/src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import { Web3ReactProvider } from "@web3-react/core"
 import { Web3Provider } from "@ethersproject/providers";
-import reportWebVitals from './reportWebVitals';
 import App from './App';
 
 
@@ -16,15 +14,9 @@ function getLibrary(provider) {
 
 
 ReactDOM.render(
-  <React.StrictMode>
     <Web3ReactProvider getLibrary={getLibrary}>
             <App />
-          </Web3ReactProvider>
-  </React.StrictMode>,
+          </Web3ReactProvider>,
+
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
